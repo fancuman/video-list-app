31ab5deed5a310ed0ab36a35baca661e644ccf15
higer <higer@gmail.com>
2019-09-09 21:40:46 +1000

    basic layout commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="ui field">
+            <input />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
