644305d4338f98dabfee24bf57b51b5751650df1
higer <higer@gmail.com>
2019-09-10 23:44:41 +1000

    use iframe to show video

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -2,6 +2,17 @@ import React from "react";
 
 export default function VideoDetails(props) {
   if (!props.video) return <div>Loading</div>;
-  const description = props.video.snippet.description;
-  return <div className="ui segment">{description}</div>;
+  const { title, description } = props.video.snippet;
+  const videoAddr = "https://www.youtube.com/embed/" + props.video.id.videoId;
+  return (
+    <div>
+      <div className="ui segment">
+        <div className="ui embed">
+          <iframe src={videoAddr} />
+        </div>
+        <h4 className="ui header">{title}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
